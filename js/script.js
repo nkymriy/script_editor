5b0877a3006a1e397fb5e195673e79168d3a4cc8
Reiya Nakayama <nkymriy@gmail.com>
2023-02-08 13:35:59 +0900

    Update 一覧をJSONから読み取るように変更

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 $(window).on("load", function () {
+	read_json();
 	$(".sortable-list").sortable({
 		axis: "y",
 		containment: $("#table-wrapper"),
@@ -8,7 +9,7 @@ $(window).on("load", function () {
 
 	$("#test-button").on("click", function () {
 		console.log("");
-		$(".sortable-list").find("[data-type='text']").each(function (i) {
+		$(".sortable-list").find("[data-type='title']").each(function (i) {
 			console.log((i + 1) + $(this).text());
 		});
 	});
@@ -16,10 +17,10 @@ $(window).on("load", function () {
 	$("#is-output-file").on("click", function () { update_console($(".sortable-list")) });
 });
 
-function update_console(jObject) {
+function update_console($object) {
 	$console = $(".console-space");
 	$console.text("");
-	$(jObject).children().each(function () {
+	$($object).children().each(function () {
 		command = $(this).find("[data-type='command']").text();
 		if ($("#is-output-file").is(":checked")) {
 			command += " >> " + $("#output-file-name").val();
@@ -27,4 +28,34 @@ function update_console(jObject) {
 		}
 		$console.html($console.html() + command + "<br>");
 	})
+}
+
+function read_json(){
+	let jsonData
+	fetch('./data.json')
+	.then(response => {
+		return response.json();
+	})
+	.then(function(json){
+		console.log(json);
+		for (const data of json.data){
+			if(data.os == "RHEL8.6"){
+				var index = 1;
+				for (const command of data.commands){
+					console.log(command.id)
+					var tr = 
+					`<tr data-no="${index}">
+						<th data-type="id">${command.id}</th>
+						<td data-type="title">${command.title}</td>
+						<td data-type="command">${command.command}</td>
+					`
+					$(tr).appendTo($(".sortable-list"));
+					index += 1;
+				}
+			}
+		}
+		// 初回アップデート用
+		update_console($(".sortable-list"));
+	});
+	return jsonData;
 }
