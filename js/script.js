a2dfad9613d8b3c19a08e0239432c4f33a82969c
Reiya Nakayama <nkymriy@gmail.com>
2023-02-08 11:58:22 +0900

    UPDATE ドラッグ時の挙動修正、出力対応

--- js/script.js
+++ js/script.js
@@ -1,10 +1,30 @@
-$(window).on("load", function(){
-	$(".sortable-list").sortable();
-	
-	$("#test-button").on("click", function(){
+$(window).on("load", function () {
+	$(".sortable-list").sortable({
+		axis: "y",
+		containment: $("#table-wrapper"),
+		create: function (event, ui) { update_console(this); },
+		deactivate: function (event, ui) { update_console(this); }
+	});
+
+	$("#test-button").on("click", function () {
 		console.log("");
-		$(".sortable-list").find("[data-type='text']").each(function(i){
-			console.log((i+1) + $(this).text());
+		$(".sortable-list").find("[data-type='text']").each(function (i) {
+			console.log((i + 1) + $(this).text());
 		});
 	});
+
+	$("#is-output-file").on("click", function () { update_console($(".sortable-list")) });
 });
+
+function update_console(jObject) {
+	$console = $(".console-space");
+	$console.text("");
+	$(jObject).children().each(function () {
+		command = $(this).find("[data-type='command']").text();
+		if ($("#is-output-file").is(":checked")) {
+			command += " >> " + $("#output-file-name").val();
+
+		}
+		$console.html($console.html() + command + "<br>");
+	})
+}
